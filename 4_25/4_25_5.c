501788d02ae26f2d143d9b6e457e313093467cce
liny0329 <jhm-250-rgg@e-net.nara.jp>
2025-05-02 05:30:01 +0000

    6.c

--- 4_25/4_25_5.c
+++ 4_25/4_25_5.c
@@ -4,9 +4,9 @@ int main() {
     int score;
     printf("点数は？");
     scanf("%d",&score);
-     if("score>=80");{
+     if("score>=80"){
     printf("合格￥n");
-}else{
+     }else{
     printf("不合格￥n");}
     return 0;
 }
